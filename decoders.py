f51448af966d37e7eddb37a8d9be12195020e6c9
Stefan Lazov <stefanlazov@dhcp-10-248-103-77.eduroam.wireless.private.cam.ac.uk>
2020-02-07 16:51:44 +0000

    TEST

--- decoders.py
+++ decoders.py
@@ -198,7 +198,7 @@ class NMTDecoder(nn.Module):
             score_for_y_t_index = self.classifier(F.dropout(prediction_vector, 0.3, training=self.training_mode))
             
             # ## Linear classifier on top of the prediction vector - If factorization method is not utilized
-            embedding_prediction = self.classifier(F.dropout(prediction_vector, 0.3, training=self.training_mode))
+            #embedding_prediction = self.classifier(F.dropout(prediction_vector, 0.3, training=self.training_mode))
 
             # # Get the indeces of all words included in the target vocab
             # # all_target_vocab_indices = []
@@ -208,7 +208,7 @@ class NMTDecoder(nn.Module):
             all_target_vocab_indices = torch.arange(0, self.num_embeddings, dtype=torch.long).unsqueeze(0).repeat(batch_size, 1).to(encoder_state.device)
 
             # # Convert the indices to a torch tensor
-            all_target_vocab_indices = torch.LongTensor(all_target_vocab_indices).to(encoder_state.device)
+            all_target_vocab_indices = torch.LongTensor(all_target_vocab_indices).to( .device)
 
             # # This should effectively return a tensor of shape BSxTarget_Vocab_SizexEmb_Dim
             target_vocab_embeddings = self.target_embedding(all_target_vocab_indices)
