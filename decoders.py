d9bde6999625f51e7b80a9890239891ce47d0c61
Stefan Lazov <stefanlazov@dhcp-10-248-103-77.eduroam.wireless.private.cam.ac.uk>
2020-02-07 14:45:31 +0000

    Change

--- decoders.py
+++ decoders.py
@@ -200,9 +200,9 @@ class NMTDecoder(nn.Module):
             #     all_target_vocab_indices.append(list(range(self.num_embeddings)))
 
             all_target_vocab_indices = torch.arange(0, self.num_embeddings, dtype=torch.long).unsqueeze(0).repeat(batch_size)
-            
+
             # Convert the indices to a torch tensor
-            all_target_vocab_indices = torch.LongTensor(all_target_vocab_indices).to(encoder_state.device)
+            #all_target_vocab_indices = torch.LongTensor(all_target_vocab_indices).to(encoder_state.device)
 
             # This should effectively return a tensor of shape BSxTarget_Vocab_SizexEmb_Dim
             target_vocab_embeddings = self.target_embedding(all_target_vocab_indices)
