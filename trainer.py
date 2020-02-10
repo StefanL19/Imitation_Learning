53f73b64bc3807155b55b83e4b2b7de622abecef
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2020-02-10 09:58:25 +0000

    Better accuracy

--- trainer.py
+++ trainer.py
@@ -112,10 +112,13 @@ def compute_accuracy(y_pred, y_true, mask_index, batch_index):
     # print(y_true.shape)
 
     # Consider only the unigram probs when measuring the accuracy
-    y_pred = y_pred[:][:][:3000]
+    y_pred = y_pred[:, :, :3000]
 
     _, y_pred_indices = y_pred.max(dim=-1)
     _, y_true_indices = y_true.max(dim=-1)
+
+    print(y_pred_indices)
+
 
     correct_indices = torch.eq(y_pred_indices, y_true_indices).float()
     valid_indices = torch.ne(y_true_indices, mask_index).float()
